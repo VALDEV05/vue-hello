48eb1ec2fee1341204ae8ba18ac68dce1a0d06ba
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-28 15:03:29 +0200

    Initial Commit
    Completed the folder structure and created the instance with #app

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+
+
+
+//creare un istanza (collegamento tra HTML E JS)
+//con le varie classi
+var app = new Vue({
+    el: '#app',
+    data: {
+
+
+    }
+})
